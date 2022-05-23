d27db0ccf6affec44fd92359a99227b00f79e426
Nitesh <kumarniteshsah1999@gmail.com>
2022-05-23 10:25:22 +0530

    First commit

---
+++ Practice/src/Basic.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any number from the keyboard");
+		int a=sc.nextInt();
+		System.out.println("You have enterd "+a);
+		
+
+	}
+
+}
