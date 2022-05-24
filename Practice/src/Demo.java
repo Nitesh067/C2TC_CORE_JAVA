66b509667acc42830111980140f7df5cf9deedb9
Nitesh <kumarniteshsah1999@gmail.com>
2022-05-24 21:04:08 +0530

    First commit

---
+++ Practice/src/Demo.java
@@ -0,0 +1,11 @@
+public class Demo{
+	int x;
+	public static void main(String args[]) {
+		Demo MyObj1=new Demo();
+		Demo MyObj2=new Demo();
+		MyObj1.x=30;
+		MyObj2.x=40;
+		System.out.println(MyObj1.x);
+		System.out.println(MyObj2.x);
+	}
+}
