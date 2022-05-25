99f00016fabfe1b472a6d3495b339f7d6e61f1ca
Nitesh <kumarniteshsah1999@gmail.com>
2022-05-25 23:08:10 +0530

    First commit

---
+++ Practice/src/Operators.java
@@ -0,0 +1,12 @@
+
+public class Operators {
+	public static void main(String args[]) {
+		int a=10;
+		int b=20;
+		int c=15;
+		System.out.println(a<b&&b>c);
+		System.out.println(a<b||a<c);
+		System.out.println(a^b&b^c);
+	}
+
+}
