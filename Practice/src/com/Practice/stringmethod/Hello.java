6729133dadf5d7e01f19ac8492fb28be2b6ab8c2
Nitesh <kumarniteshsah1999@gmail.com>
2022-06-02 20:04:00 +0530

    first commit

---
+++ Practice/src/com/Practice/stringmethod/Hello.java
@@ -0,0 +1,16 @@
+package com.Practice.stringmethod;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		String s="Hello";
+		if(s.length()==5) {
+			System.out.println("The length is "+s.length());
+		}
+		else {
+			System.out.println("The length is more then "+s.length());
+		}
+
+	}
+
+}
