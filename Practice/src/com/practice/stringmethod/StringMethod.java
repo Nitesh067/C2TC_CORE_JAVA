136ec3771e5c8d702ec68a9dc94153fdf80aa86d
Nitesh <kumarniteshsah1999@gmail.com>
2022-06-02 15:31:51 +0530

    first commit

---
+++ Practice/src/com/practice/stringmethod/StringMethod.java
@@ -0,0 +1,12 @@
+package com.practice.stringmethod;
+
+public class StringMethod {
+
+	public static void main(String[] args) {
+		String s="Nitesh Kumar";
+		String s1="Kumar Nirtesh";
+		System.out.println(s.equals(s1));
+
+	}
+
+}
